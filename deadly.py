59cd01db1da7dc14a4c502e981c36217d9cd0a90
thunderxgo <thisisforworkandbusiness19@gmail.com>
2025-12-01 22:33:43 +1100

    add more detail on no. rolled

--- deadly.py
+++ deadly.py
@@ -3,9 +3,9 @@ import time
 
 def funny(theInt):
     if theInt == 6:
-        print("you lose bucko")
+        print("you lose bucko (your harddrive is also gone), you rolled " + str(theInt))
     else:
-        print("you win")
+        print("you win, you rolled " + str(theInt))
 
 alright = random.randint(1, 6)
 
