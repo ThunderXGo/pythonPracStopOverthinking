6328b91fa9ef35a0dee966e8a4905544d2ecb4ec
thunderxgo <thisisforworkandbusiness19@gmail.com>
2025-12-08 22:28:02 +1100

    initial given for problem 2

--- intro/helloInvisible.py
+++ intro/helloInvisible.py
@@ -1,3 +1,33 @@
-#add code to print 'In a hole in the ground there lived a hobbit.'
+"""
+Loudspeaker announcements are often repeated to make sure people hear them!
 
-print("In a hole in the ground there lived a hobbit.")
+Here's a program that repeats announcements for you.
+
+1.
+Run the program to see what it does.
+
+The message variable is used twice to repeat the announcement.
+
+2.
+Update the program so that it announces something different:
+
+Free ice cream!
+I repeat:
+Free ice cream!
+​
+You'll need to update the message variable, by setting it to the new value!
+
+3.
+Mark your program, fix any errors, run and mark it again.
+
+Comments
+The red lines starting with a hash (#) are comments. They are ignored when you run the code.
+"""
+
+# Set the message:
+message = "Bart Simpson to the principal's office!"
+
+# Print the announcement:
+print(message)
+print('I repeat:')
+print(message)
