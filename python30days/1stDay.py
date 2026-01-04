0b81f360ef16f92c51b180e2dd511d0cea141f22
thunderxgo <thisisforworkandbusiness19@gmail.com>
2026-01-05 00:15:11 +1100

    operand calcs 3 and 4 done

--- python30days/1stDay.py
+++ python30days/1stDay.py
@@ -2,4 +2,10 @@
 #use python --version to check version
 
 #Calculations with operand 3 and 4:
-print(3 + 4)
+print(f"addition: {3 + 4} ")
+print(f"subtraction: {3 - 4} ")
+print(f"multiplication: {3 * 4} ")
+print(f"modulus/remainder: {3 % 4} ")
+print(f"division: {3 / 4} ")
+print(f"exponential: {3 ** 4} ")
+print(f"floor division: {3 // 4} ")
