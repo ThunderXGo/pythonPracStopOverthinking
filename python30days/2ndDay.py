610e311362d336b440a44f3899b403a7f355f094
thunderxgo <thisisforworkandbusiness19@gmail.com>
2026-01-05 21:52:01 +1100

    it worked just as i expected, combining two strs with a space

--- python30days/2ndDay.py
+++ python30days/2ndDay.py
@@ -1,4 +1,11 @@
 #Exercise 2
-#1. musta be variable setting
 #im not gonna bother listing all subparts of a subpart as theres over 10 brochachos
+#1. musta be variable setting
+
+
+firstName = "John"
+lastName = " Doe"
+fullName = firstName + lastName
+print(fullName)
+
 
