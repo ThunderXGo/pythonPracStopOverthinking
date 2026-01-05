728fb6d9a71b06b09f5672d1b2282cfa4c8d1c62
thunderxgo <thisisforworkandbusiness19@gmail.com>
2026-01-05 21:25:49 +1100

    data type check working

--- python30days/1stDay.py
+++ python30days/1stDay.py
@@ -20,3 +20,10 @@ print(f"Country: Headspace")
 print(f"Ehhh 30 day python gonna be a piece of cake")
 """
 
+#4. Check data type 
+print(type(10))
+print(type(9.8))
+print(type(3.14))
+print(type(4-4j))
+print(type(['Asabeneh', 'Python', 'Finland']))
+print(type("ehh imma skip"))
