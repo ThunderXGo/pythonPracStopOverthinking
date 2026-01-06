d0118aecefff19f71339440c4d19392d266a35bf
thunderxgo <thisisforworkandbusiness19@gmail.com>
2026-01-06 22:35:36 +1100

    calc for rect area + peri done

--- python30days/3rdDay.py
+++ python30days/3rdDay.py
@@ -10,9 +10,16 @@ b = int(input("Enter base of triangle: "))
 h = int(input("Enter height of triangle: "))
 areaOfTri = (b * h) / 2
 print(f"The area of triangle is: {areaOfTri}")
-"""
+
 sideA = int(input("Enter side A of triangle: "))
 sideB = int(input("Enter side B of triangle: "))
 sideC = int(input("Enter side C of triangle: "))
 periOfTri = sideA + sideB + sideC
 print(f"The perimeter of triangle is: {periOfTri}")
+"""
+lenRect = int(input("Enter length of rect: "))
+widthRect = int(input("Enter width of rect: "))
+areaOfRect = lenRect * widthRect
+periOfRect = 2 * (lenRect + widthRect)
+print(f"The area of rect is: {areaOfRect}")
+print(f"The perimeter of rect is: {periOfRect}")
