2c2f726ed0f1cd8f681d103c3814c9f733cd94e7
thunderxgo <thisisforworkandbusiness19@gmail.com>
2026-01-06 22:44:21 +1100

    sneaky import math to get accurate pi value, otherwise circ stuff done

--- python30days/3rdDay.py
+++ python30days/3rdDay.py
@@ -1,3 +1,4 @@
+import math
 #Exercise 3
 
 #1. More variable and complex math i assume?
@@ -16,10 +17,17 @@ sideB = int(input("Enter side B of triangle: "))
 sideC = int(input("Enter side C of triangle: "))
 periOfTri = sideA + sideB + sideC
 print(f"The perimeter of triangle is: {periOfTri}")
-"""
+
 lenRect = int(input("Enter length of rect: "))
 widthRect = int(input("Enter width of rect: "))
 areaOfRect = lenRect * widthRect
 periOfRect = 2 * (lenRect + widthRect)
 print(f"The area of rect is: {areaOfRect}")
 print(f"The perimeter of rect is: {periOfRect}")
+"""
+r = int(input("Enter radius of circle: "))
+pi = math.pi
+areaOfCirc = pi * (r**2)
+circumferenceOfCirc = 2 * pi * r
+print(f"The area of circ is: {areaOfCirc}")
+print(f"The perimeter of rect is: {circumferenceOfCirc}")
