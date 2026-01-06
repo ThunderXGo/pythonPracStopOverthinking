8da35178395745032f168f30b22b1edfda7b1765
thunderxgo <thisisforworkandbusiness19@gmail.com>
2026-01-06 22:31:30 +1100

    calc for tri perimeter done

--- python30days/3rdDay.py
+++ python30days/3rdDay.py
@@ -1,6 +1,7 @@
 #Exercise 3
 
 #1. More variable and complex math i assume?
+"""
 age = 21
 height = 190.6
 complicated = 10j
@@ -9,3 +10,9 @@ b = int(input("Enter base of triangle: "))
 h = int(input("Enter height of triangle: "))
 areaOfTri = (b * h) / 2
 print(f"The area of triangle is: {areaOfTri}")
+"""
+sideA = int(input("Enter side A of triangle: "))
+sideB = int(input("Enter side B of triangle: "))
+sideC = int(input("Enter side C of triangle: "))
+periOfTri = sideA + sideB + sideC
+print(f"The perimeter of triangle is: {periOfTri}")
