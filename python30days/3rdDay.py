1cea23c6ca1d7662588c3440e74a007d697c4d42
thunderxgo <thisisforworkandbusiness19@gmail.com>
2026-01-06 22:29:36 +1100

    triangle calc using user input, remember to conv str to ints man

--- python30days/3rdDay.py
+++ python30days/3rdDay.py
@@ -3,4 +3,9 @@
 #1. More variable and complex math i assume?
 age = 21
 height = 190.6
-print(type(height))
+complicated = 10j
+
+b = int(input("Enter base of triangle: "))
+h = int(input("Enter height of triangle: "))
+areaOfTri = (b * h) / 2
+print(f"The area of triangle is: {areaOfTri}")
