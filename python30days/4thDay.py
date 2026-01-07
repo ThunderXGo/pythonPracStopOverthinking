09254bdfd6736ac9f8f2944b5ed4244138557482
thunderxgo <thisisforworkandbusiness19@gmail.com>
2026-01-07 23:39:13 +1100

    kind of niche str functs

--- python30days/4thDay.py
+++ python30days/4thDay.py
@@ -8,8 +8,12 @@ print(index[0] + index[1] + index[2] + index[3])
 index2 = ["Coding", " for", " all"]
 print(index2[0] + index2[1] + index2[2])
 """
-company = "Coding for all"
+company = "Coding for All"
 print(company)
 print(len(company))
 print(str.upper(company))
 print(str.lower(company))
+
+print(str.capitalize(company))
+print(str.title(company))
+print(str.swapcase(company))
