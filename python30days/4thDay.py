41f468c20d9fbde3f559548cc286093373b302c7
thunderxgo <thisisforworkandbusiness19@gmail.com>
2026-01-07 23:35:38 +1100

    conversion with inbuilt str func

--- python30days/4thDay.py
+++ python30days/4thDay.py
@@ -2,7 +2,14 @@ import math
 #Exercise 4
 
 #1. working with strings
+"""
 index = ["Thirty", " days", " of", " python"]
 print(index[0] + index[1] + index[2] + index[3])
 index2 = ["Coding", " for", " all"]
 print(index2[0] + index2[1] + index2[2])
+"""
+company = "Coding for all"
+print(company)
+print(len(company))
+print(str.upper(company))
+print(str.lower(company))
