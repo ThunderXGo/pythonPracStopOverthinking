2b0d284381a769f2be48b2cb8c328aa34dfa0387
thunderxgo <thisisforworkandbusiness19@gmail.com>
2026-01-07 23:32:37 +1100

    same exact thing w diff value

--- python30days/4thDay.py
+++ python30days/4thDay.py
@@ -4,3 +4,5 @@ import math
 #1. working with strings
 index = ["Thirty", " days", " of", " python"]
 print(index[0] + index[1] + index[2] + index[3])
+index2 = ["Coding", " for", " all"]
+print(index2[0] + index2[1] + index2[2])
