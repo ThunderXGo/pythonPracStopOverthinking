53df2915d73f50a961c7781f386be6090e88be8b
thunderxgo <thisisforworkandbusiness19@gmail.com>
2026-01-07 23:31:21 +1100

    a neat way to concatenate strs done

--- python30days/4thDay.py
+++ python30days/4thDay.py
@@ -1,5 +1,6 @@
 import math
 #Exercise 4
 
-#1. More variable and complex math i assume?
-print(3, 3, 3)
+#1. working with strings
+index = ["Thirty", " days", " of", " python"]
+print(index[0] + index[1] + index[2] + index[3])
